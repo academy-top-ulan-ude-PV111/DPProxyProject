ab4fe205f71f25caa546dada3bfb97d6bd4d6a2e
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-22 15:31:23 +0300

    Добавьте файлы проекта.

---
+++ DPProxyProject/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace DPProxyProject
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    interface IGroupUser
+    {
+        User UserGetById(int id);
+        void Close();
+    }
+    class Departament : IGroupUser
+    {
+        UserContext userContext;
+
+        public void Close()
+        {
+            UserContext.
+        }
+
+        public User UserGetById(int id)
+        {
+            return new User();
+        }
+    }
+
+    class DepartamentProxy : IGroupUser
+    {
+        List<User> users;
+        Departament departament;
+
+        public void Close()
+        {
+            // copy data to Departament
+            //Close().
+        }
+
+        public User UserGetById(int id)
+        {
+            return new User();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
